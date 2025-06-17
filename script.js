440695a91af755b736f2d9e0ed10c2255ff3a523
Crisitan <crisluis2512@gmail.com>
2025-06-17 01:05:36 -0300

    finishing the scripts

--- script.js
+++ script.js
@@ -13,19 +13,37 @@ var API_URL = "https://api.exchangerate-api.com/v4/latest/"
 async function convertMoney(){
     butao.style.display = "none"
     loading.style.display = "block"
+    error.style.display = "none"
+    result.style.display = "none"
 
     //TENTAR ACESSAR O SERVIDOR
     try{
         const response = await fetch(API_URL + fromCurrency.value)
         const data = await response.json()
-        //console.log(data);
-        //const rate = data.rates.USD
-        console.log(toCurrency);
-        
+
+        const rate = data.rates[toCurrency.value]
+        const convertedRate = (amount.value * rate).toFixed(2)
+
+        convertedAmount.value = convertedRate
+        error.style.display = "none"
+        result.style.display = "block"
+
+        result.innerHTML = `
+        <div style="font-size: 1.4rem;"> 
+            ${amount.value} ${fromCurrency.value} = ${convertedAmount.value} ${toCurrency.value}
+        <div/>
+
+        <div style="font-size: 0.9rem; opacity: 0.8; margin-top: 10px;"> 
+            Taxa: 1 ${fromCurrency.value} = ${rate}
+        <div/>
+        `
     }
-    catch(error){
-        alert("Falha no servidor!")
+    catch(err){
+        error.style.display = "block"
+        error.innerHTML = 'Falha ao converter moeda! Tente novamente.'
     }
+
+    loading.style.display = "none"
 }
 
 fomula.addEventListener("submit", function (event){
